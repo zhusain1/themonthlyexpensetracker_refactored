a91092a9846de9f2cbf8dd5b9f3950e819f26db5
zhusain1 <zhusain1@umbc.edu>
2022-09-05 19:45:58 -0400

    src/Functional_components/BankSetup.js

--- src/Functional_components/BankSetup.js
+++ src/Functional_components/BankSetup.js
@@ -7,6 +7,7 @@ import api from '../Api/Api';
 export default function BankSetup(){
 
   const [token, setToken] = useState('')
+  const isOAuthRedirect = window.location.href.includes('?oauth_state_id=');
 
   const navigate = useNavigate();
 
@@ -18,6 +19,11 @@ export default function BankSetup(){
           navigate('/')
       }
 
+      if (isOAuthRedirect) {
+        setToken(sessionStorage.getItem('link_token'));
+        return;
+      }
+
       api.post('/user/validate/token')
           .then(() => {
                // Make request to get token
@@ -27,6 +33,9 @@ export default function BankSetup(){
                     let { link_token } = response.data;
             
                     setToken(link_token)
+
+                    // store link_token temporarily in case of OAuth redirect
+                    sessionStorage.setItem('link_token', link_token);
             
                 }catch (error) {
                   console.log(error)
@@ -54,6 +63,7 @@ export default function BankSetup(){
     const publicTokenRequest = async () => {
       try{
           const response = await api.post('/plaid/exchange', request);
+          console.log('token exchange')
           console.log(response.data);
           navigate('/account');
       }catch (error) {
@@ -63,18 +73,48 @@ export default function BankSetup(){
     publicTokenRequest();
   },[navigate]);
 
-  const { open, ready } = usePlaidLink({
+
+  const config = {
+    // token must be the same token used for the first initialization of Link
     token,
     onSuccess,
-  });
+  };
+
+
+  if (isOAuthRedirect) {
+    // receivedRedirectUri must include the query params
+    config.receivedRedirectUri = window.location.href;
+  }
+
+
+  const { open, ready } = usePlaidLink(config);
+
+  const displayButton  = () => {
+    if(!isOAuthRedirect){
+      return(
+        <div>
+          <h2> Setup Account</h2>
+          <p> Please connect to a bank to setup your account.</p>
+          <Button onClick={() => open()} disabled={!ready}>
+            Connect to bank
+          </Button>
+        </div>
+      );  
+    } else{
+      return(
+        <div>
+          <h2> Setup Account</h2>
+          <p> Almost done, select button to finish setting up your account.</p>
+          <Button onClick={() => open()} disabled={!ready}>
+            Authorize accounts
+          </Button>
+        </div>
+      );  
+    }
+  }
+
 
   return(
-    <div>
-      <h2> Setup Account</h2>
-      <p> Please connect to a bank to setup your account.</p>
-      <Button onClick={() => open()} disabled={!ready}>
-        Connect to bank
-      </Button>
-    </div>
+    displayButton()
   );  
 }
